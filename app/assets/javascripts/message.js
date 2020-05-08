bbedf4220ebca71799d368c8d3e1131134f9035d
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-05-08 13:20:10 +0900

    インデントを修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -58,9 +58,9 @@ $('.new_message').on('submit', function(e){
     $('form')[0].reset();
     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
     $(".form__submit-btn").prop('disabled', false);
+    })
+  　.fail(function() {
+   　 alert("メッセージ送信に失敗しました");
+  　});
   })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-});
-})
 });
