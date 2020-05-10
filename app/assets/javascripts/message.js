382d0dd43070b57242c3e2ddf36b09adfcf7a9f3
yuki-bizz <yuki.bizz.8011@gmail.com>
2020-05-10 16:49:21 +0900

    自動更新機能　修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,6 @@
 $(function() {
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
